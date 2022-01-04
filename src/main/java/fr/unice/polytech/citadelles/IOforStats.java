27d9fe5f0f99b4fccd5c9ad53d2f73efdd3aa808
Florian LATAPIE <florian.latapie@etu.unice.fr>
2022-01-04 08:58:37 +0100

    ajout de la moyenne des points sur les 1000 parties #64

--- src/main/java/fr/unice/polytech/citadelles/IOforStats.java
+++ src/main/java/fr/unice/polytech/citadelles/IOforStats.java
@@ -18,21 +18,22 @@ public class IOforStats extends IO {
 
     public void printStats(List<Player> players, List<List<Player>> winnersOfEachGame) {
         Map<Player, Integer> statsForEachPlayer = new HashMap<>();
-        for (Player p: players) {
-            statsForEachPlayer.put(p,0);
+        for (Player p : players) {
+            statsForEachPlayer.put(p, 0);
         }
 
 
         for (List<Player> winnersForThisGame : winnersOfEachGame) {
-            for (Player playerToSearch: players) {
-                if (winnersForThisGame.get(0).getName().equals(playerToSearch.getName())){
-                    statsForEachPlayer.put(playerToSearch, statsForEachPlayer.get(playerToSearch)+1);
+            for (Player playerToSearch : players) {
+                if (winnersForThisGame.get(0).getName().equals(playerToSearch.getName())) {
+                    statsForEachPlayer.put(playerToSearch, statsForEachPlayer.get(playerToSearch) + 1);
                 }
             }
         }
 
-        for (Player p: players) {
-            System.out.println(p.getName()+ " has won "+statsForEachPlayer.get(p) + " games with strategy :" + p.getStrategy());
+        for (Player p : players) {
+            System.out.println(p.getName() + " has won "
+                    + statsForEachPlayer.get(p) + " games with strategy :" + p.getStrategy());
         }
     }
 }
